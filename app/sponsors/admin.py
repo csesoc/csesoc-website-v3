c06321dd54d1ef4489ac3478733a5f6e2e05bbe5
Christopher Manouvrier <cmanou@mac.com>
2014-02-03 16:25:16 +1100

    [sponsors] Sponsor admin fixes

--- app/sponsors/admin.py
+++ app/sponsors/admin.py
@@ -5,7 +5,13 @@ from app.sponsors.models import Sponsor
 
 class SponsorAdmin(PageAdmin):
     list_display = ('name', 'amount_paid', 'expiry_date')
-
+    list_display_links = ('name', 'amount_paid')
+    list_editable = ()
+    list_filter = ('amount_paid', 'expiry_date')
+    radio_fields = {}
+    date_hierarchy = 'expiry_date'
+    def in_menu(self):
+        return True
 
 admin.site.register(Sponsor, SponsorAdmin)
 
--- app/sponsors/models.py
+++ app/sponsors/models.py
@@ -4,7 +4,7 @@ from mezzanine.pages.models import Page
 class Sponsor(models.Model):
     name		= models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    website 	= models.URLField(verify_exists=False)
+    website 	= models.URLField()
     logo 		= models.ImageField(upload_to='sponsors')
     amount_paid = models.PositiveIntegerField()
     start_date 	= models.DateField(auto_now_add=True, editable=True)
