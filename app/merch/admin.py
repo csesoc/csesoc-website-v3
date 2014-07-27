0dbc31e19ab4a4c84a537058c9b680087442441d
Vincent Tran <tran.vincent17@gmail.com>
2014-07-27 17:58:41 +1000

    Fixed merch invoice redirect link

--- app/merch/admin.py
+++ app/merch/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from app.merch.models import Hoodie
 
 class HoodieAdmin(admin.ModelAdmin):
-  list_filter = ('size','gender', 'payment_status')
+  list_filter = ('full_name', 'email_address', 'contact_number', 'size','gender', 'payment_status')
   list_display = ('full_name', 'email_address', 'contact_number', 'gender', 'size','payment_status')
 
 admin.site.register(Hoodie, HoodieAdmin)
--- app/merch/views.py
+++ app/merch/views.py
@@ -17,7 +17,7 @@ def hoodies(request):
       form = HoodieForm(request.POST)
       if form.is_valid():
           form.save()
-          return redirect("https://www.csesoc.unsw.edu.au/finance/20140002/dc303e8dd7937d2eee5113feb20a3fec")
+          return redirect("https://www.csesoc.unsw.edu.au/finance/20140002/8771d81806d361be65c066d71038d1d7")
     else:
       form = HoodieForm() # unbound form
     return render_to_response('merch/hoodies.html', {'form' : form}, context_instance=RequestContext(request))
