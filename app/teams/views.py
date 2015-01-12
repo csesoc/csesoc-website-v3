6ff961b46f13f3760c469ca90c9a2a84d5e926f9
CSESoc Dev Head <csesoc.dev.head@csesoc.unsw.edu.au>
2015-01-13 00:55:09 +1100

    Renamed Tech to TalkShops

--- app/teams/views.py
+++ app/teams/views.py
@@ -14,7 +14,7 @@ def mailinglist(request):
         # home dir on (ssh csesoc@cse.unsw.edu.au)
         teams = (
             ('Beta', 'beta', 'Beta is a fortnightly newsletter CSESoc puts out, containing the latest and greatest news about computing, UNSW, CSE, and the world that revolves around you, our reader.'),
-            ('Tech', 'tech', "The Tech working group organises educational events throughout the year to help CSE students (and anyone else, really) use CSE's technical resources, mostly the labs' computers."),
+            ('TalkShops', 'talkshops', "The TalkShops working group organises educational events throughout the year to help CSE students (and anyone else, really) use CSE's technical resources, mostly the labs' computers."),
             ('Social', 'social', "CSESoc is a all student run society, and being part of the social team is a great place to start getting involved in the society's operations."),
             ('Publicity', 'publicity', "Is for those that love designing things, are very vocal or just like to try new things."),
             ('Dev', 'dev', "The Dev team works on improving life in CSE through software. This could be anything from improving current systems to developing and implementing completely new systems from scratch."),
@@ -22,8 +22,8 @@ def mailinglist(request):
         )
 
         h = httplib.HTTPConnection('cgi.cse.unsw.edu.au')
-        h.request('GET', '/~csesoc/mailingLists?cseid=' + request.user.username)
-        teams_joined = h.getresponse().read().split(',')
+        h.request('GET', '/~csesoc/updateMailingLists?cseid=' + request.user.username)
+        teams_joined = h.getresponse().read().strip().split(',')
 
         joined_teams = ((team[0], team[1], team[2], team[1] in teams_joined) for team in teams)
 
@@ -35,10 +35,10 @@ def mailinglist(request):
 
 
 def join(request, team):
-    assert team in ('Beta', 'Tech', 'Social', 'Publicity', 'Dev', 'HS Team')
+    assert team in ('Beta', 'TalkShops', 'Social', 'Publicity', 'Dev', 'HS Team')
     return HttpResponse('success: ' + team)
 
 
 def leave(request, team):
-    assert team in ('Beta', 'Tech', 'Social', 'Publicity', 'Dev', 'HS Team')
+    assert team in ('Beta', 'TalkShops', 'Social', 'Publicity', 'Dev', 'HS Team')
     return HttpResponse('success: ' + team)
