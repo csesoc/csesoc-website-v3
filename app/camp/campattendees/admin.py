3a9c81b2e896187fa9263acd73d93717ec2ab006
CSESoc Dev Head <csesoc.dev.head@csesoc.unsw.edu.au>
2017-02-19 10:10:17 +1100

    Added zID to view and sort by name and zID

--- app/camp/campattendees/admin.py
+++ app/camp/campattendees/admin.py
@@ -3,8 +3,8 @@ from app.camp.campattendees.models import Application
 from app.camp import campglobals
 
 class ApplicationAdmin(admin.ModelAdmin):
-  list_filter = ('year','shirt_size','gender', 'payment_status')
-  list_display = ('full_name', 'gender', 'age', 'cse_program','shirt_size','payment_status','year', 'medical_pdf')
+  list_filter = ('full_name', 'student_number', 'year','shirt_size','gender', 'payment_status')
+  list_display = ('full_name', 'student_number', 'gender', 'age', 'cse_program','shirt_size','payment_status','year', 'medical_pdf')
   actions = [campglobals.mark_deleted, campglobals.mark_arcEarlyPaid, campglobals.mark_nonarcEarlyPaid, campglobals.mark_arcRegPaid, campglobals.mark_nonarcRegPaid, campglobals.mark_medicalyes]
 
 admin.site.register(Application, ApplicationAdmin)
